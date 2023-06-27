b5cb2b8e386384be793c932db135508410ede0d6
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-06-27 18:29:49 +0200

    Create 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server /7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# puppet manifest to install 7 configure nginx server,
+# with a 301 redirect 
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
