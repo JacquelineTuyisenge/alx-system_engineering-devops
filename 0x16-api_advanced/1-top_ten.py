14c4ba63e5e64e620c0b4786ab5a55dbf384d02b
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-08-11 01:14:02 +0200

    Create 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 1-top_ten.py """
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
